b2ce6b9c34654e92a7e76e5753eb755f994fbfce
Rafael Cortes <rafael_cortes@outlook.com>
2024-07-24 16:53:13 -0300

    Aula 147 - Arrays

---
+++ 05-javascript-dom/aula147/script.js
@@ -0,0 +1,24 @@
+// ARRAYS
+
+// -------------------------------
+// até agora...
+// -------------------------------
+let nome1 = 'joao';
+let nome2 = 'ana';
+let nome3 = 'carlos';
+
+// com um array...
+let nomes = ['joao', 'ana', 'carlos'];
+
+// -------------------------------
+// apresentar dados de um array
+// -------------------------------
+console.log(nomes); // todos os elementos
+console.log(nomes[1]); // ana
+console.table(nomes);
+
+// -------------------------------
+// alterar dados de um array
+// -------------------------------
+nomes[1] = 'maria';
+console.table(nomes);
