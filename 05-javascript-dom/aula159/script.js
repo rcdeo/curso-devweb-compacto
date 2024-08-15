3ef87cb6f38894eec13ab7b80676c52f4ad9635d
Rafael Cortes <rafael_cortes@outlook.com>
2024-08-15 20:39:38 -0300

    Aula 159 - Outros Métodos Get Elements

---
+++ 05-javascript-dom/aula159/script.js
@@ -0,0 +1,15 @@
+/*
+document.getElementsByClassName()
+document.getElementsByName()
+document.getElementsByTagName()
+*/
+
+// let e = document.getElementsByClassName('outra-noticia');
+// // console.log(e);
+// e[0].innerHTML = 'Nova notícia';
+
+// let e = document.getElementsByName('text_usuario');
+// e[0].value = 'João';
+
+let e = document.getElementsByTagName('div');
+console.log(e);
