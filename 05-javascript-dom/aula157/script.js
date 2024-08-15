d9b00b90e654d2a8aea81d924fb0ca4eb2720714
Rafael Cortes <rafael_cortes@outlook.com>
2024-08-15 11:05:58 -0300

    Aula 157 - Introdução à navegação no DOM

---
+++ 05-javascript-dom/aula157/script.js
@@ -0,0 +1,10 @@
+// NAVEGAR NO DOM
+
+let elemento = document.documentElement;
+// console.log(elemento);
+// console.log(typeof elemento);
+// console.log(elemento.lang);
+
+elemento = document.head;
+console.log(elemento);
+// console.table(elemento);
