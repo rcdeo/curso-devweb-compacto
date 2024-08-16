ebb895d11b67c3f9c4ee0eafeb8ff08c857a47ec
Rafael Cortes <rafael_cortes@outlook.com>
2024-08-15 21:14:58 -0300

    Aula 160 - Introdução ao querySelector & querySelectorAll

---
+++ 05-javascript-dom/aula160/script.js
@@ -0,0 +1,12 @@
+/*
+document.querySelector()
+document.querySelectorAll()
+*/
+
+// let e = document.querySelector('#titulo');
+// console.log(e);
+
+let e = document.querySelector('p');
+let a = document.querySelectorAll('p');
+console.log(e);
+console.log(a);
