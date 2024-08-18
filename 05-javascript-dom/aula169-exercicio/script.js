fc7e03a16c933c7f8cafcfab38381a20a4f37a5a
Rafael Cortes <rafael_cortes@outlook.com>
2024-08-18 17:22:40 -0300

    Aula 169 - Exercício com EventListener - Event stopPropagation

---
+++ 05-javascript-dom/aula169-exercicio/script.js
@@ -0,0 +1,26 @@
+/* 
+OBJETIVO:
+Adicionar 3 eventos click, um para cada elemento dentro do body 
+cada um vai apresentar um texto único na consola
+e fazer com que sejam independentes uns dos outros.
+(Propagation)
+*/
+
+let section = document.querySelector('section');
+let article = document.querySelector('article');
+let div = document.querySelector('div');
+
+section?.addEventListener('click', (event) => {
+    event.stopPropagation();
+    console.log('section');
+});
+
+article?.addEventListener('click', (event) => {
+    event.stopPropagation();
+    console.log('article');
+});
+
+div?.addEventListener('click', (event) => {
+    event.stopPropagation();
+    console.log('div');
+});
