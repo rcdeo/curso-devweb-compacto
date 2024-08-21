e05ac531da01c31640f9ba9217ba91d8e96761ee
Rafael Cortes <rafael_cortes@outlook.com>
2024-08-21 10:51:34 -0300

    Exercício 007 - Exercício seis com código mais reduzido

---
+++ 06-exercicios-javascript/exercicio_007/app.js
@@ -0,0 +1,19 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 006
+Enunciado:
+    O mesmo exercício do vídeo anterior, mas com código mais sintético.
+---------------------------------------------------------------------------- */
+
+document.querySelectorAll('[id^="info"]').forEach((el) => {
+    el.classList.add('d-none');
+});
+document.querySelector('#info1').classList.remove('d-none');
+document.querySelectorAll('[id^="tab"]').forEach((btn) => {
+    btn.addEventListener('click', (btn) => {
+        document.querySelectorAll('[id^="info"]').forEach((el) => {
+            el.classList.add('d-none');
+        });
+        document.querySelector('#info' + btn.target.id.substring(3)).classList.remove('d-none');
+    });
+});
