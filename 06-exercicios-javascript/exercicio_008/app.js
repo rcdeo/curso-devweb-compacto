29d1d0ddde916ccead65ee43d9f5531ca0c7489c
Rafael Cortes <rafael_cortes@outlook.com>
2024-08-21 14:03:35 -0300

    Exercício 008 - Cinco botões com cores diferentes

---
+++ 06-exercicios-javascript/exercicio_008/app.js
@@ -0,0 +1,22 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 008
+Enunciado:
+    Existem 5 botões, cada um com uma cor diferente do Bootstrap.
+    A ideia é criar o código javascript que permita que o clique em cada
+    um dos botões altere o texto acima para a cor de fundo do botão.
+---------------------------------------------------------------------------- */
+
+// document.querySelectorAll('[id^="btn"]').forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//         document.querySelector('h3').className = 'text-' + e.target.className.substring(8);
+//     });
+// });
+
+const cores = ['primary', 'info', 'success', 'danger', 'warning'];
+document.querySelectorAll('[id^="btn"]').forEach((btn) => {
+    btn.addEventListener('click', (btn) => {
+        let index = btn.target.id.substring(3, 4) - 1;
+        document.querySelector('h3').className = 'text-' + cores[index];
+    });
+});
