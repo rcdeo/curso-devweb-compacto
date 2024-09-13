3523b0f9f90486b2871005ee8c2c440e54e2bb7f
Rafael Cortes <rafael_cortes@outlook.com>
2024-09-13 11:54:30 -0300

    Exercício 022 - Select determina se checkboxes estão ativas ou não

---
+++ 06-exercicios-javascript/exercicio_022/app.js
@@ -0,0 +1,30 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 022
+Enunciado:
+    Está disponível um elemento select com 3 opções.
+    Por baixo existem 4 checkboxes.
+    Quando é selecionada a opção 3 no select, as checkboxes devem ficar ativas.
+    Qualquer outra opção, devem desativar as checkboxes.
+---------------------------------------------------------------------------- */
+checks_state('disabled');
+
+document.querySelector('[name="select_opcoes"]').addEventListener('change', (e) => {
+    if (e.target.value == 3) {
+        checks_state('enabled');
+    } else {
+        checks_state('disabled');
+    }
+});
+
+function checks_state(state) {
+    if (state == 'disabled') {
+        document.querySelectorAll('[id^="check_"]').forEach((e) => {
+            e.setAttribute('disabled', true);
+        });
+    } else {
+        document.querySelectorAll('[id^="check_"]').forEach((e) => {
+            e.removeAttribute('disabled');
+        });
+    }
+}
