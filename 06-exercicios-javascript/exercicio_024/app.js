ff56fef4366430025a6dcacbcc09213598cbfa3c
Rafael Cortes <rafael_cortes@outlook.com>
2024-09-14 16:49:05 -0300

    Exercício 024 - Permissões definidas a partir de perfil em select

---
+++ 06-exercicios-javascript/exercicio_024/app.js
@@ -0,0 +1,47 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 024
+Enunciado:
+    Existem 8 checkboxes referentes a um conjunto de permissões de usuário.
+    Cada uma delas pode ser checkada individualmente.
+    Por baixo, existe um select com 3 perfis de usuário: administrador, coordenador e operador.
+    Ao selecionar um perfil, as checkboxes devem ser checkadas de acordo com o
+    perfil selecionado.
+
+    Administrador: 
+        Todas as permissões
+
+    Coordenador: 
+        Coordenação dos trabalhos
+        Gestão de calendário
+        Utilização das máquinas
+        Utilização das ferramentas
+
+    Operador:
+        Utilização das máquinas
+        Utilização das ferramentas
+
+    NOTA: sem perfil, todas as checkboxes devem estar descheckadas.
+---------------------------------------------------------------------------- */
+document.querySelector('#select_perfil').addEventListener('change', () => {
+    const perfil = document.querySelector('#select_perfil').value;
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+
+    if (perfil == 'administrador') {
+        checkboxes.forEach((checkbox) => {
+            checkbox.checked = true;
+        });
+    } else if (perfil == 'coordenador') {
+        checkboxes[4].checked = true;
+        checkboxes[5].checked = true;
+        checkboxes[6].checked = true;
+        checkboxes[7].checked = true;
+    } else if (perfil == 'operador') {
+        checkboxes[6].checked = true;
+        checkboxes[7].checked = true;
+    }
+});
