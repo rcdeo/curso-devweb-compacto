786cb11b900e509e5ad24e3813101723378ba817
Rafael Cortes <rafael_cortes@outlook.com>
2024-09-15 15:48:22 -0300

    Exercício 025 - Apresentar a tabuada a partir de número inserido em input number

---
+++ 06-exercicios-javascript/exercicio_025/app.js
@@ -0,0 +1,27 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 025
+Enunciado:
+    O input number text_numero é o multiplicando. O seu valor só pode variar
+    entre 1 e 20. Ao alterar o valor, deve ser apresentada a tabuada do 
+    número definido no div cujo id = resultados.
+---------------------------------------------------------------------------- */
+const resultados = document.querySelector('#resultados');
+const text_numero = document.querySelector('#text_numero');
+
+text_numero.setAttribute('min', 1);
+text_numero.setAttribute('max', 20);
+
+tabuada(1);
+
+text_numero.addEventListener('change', () => {
+    tabuada(text_numero.value);
+});
+
+function tabuada(valor) {
+    let html = '';
+    for (let i = 1; i <= 10; i++) {
+        html += `${valor} x ${i} = ${valor * i}<br>`;
+    }
+    resultados.innerHTML = html;
+}
