1339dfa901c671ca1695ba8d99353e1147eaad58
Rafael Cortes <rafael_cortes@outlook.com>
2024-09-16 10:44:09 -0300

    Exercício 028 - Gerar 20 números aleatórios a parti de um click

---
+++ 06-exercicios-javascript/exercicio_028/app.js
@@ -0,0 +1,17 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 028
+Enunciado:
+    Existe um botão o qual, ao ser clicado, cria 20 números aleatórios entre 1 e 1000.
+    Esses números devem ser apresentados cada um em sua própria linha dentro do elemento
+    cujo id é igual a "numeros".
+---------------------------------------------------------------------------- */
+document.querySelector('button').addEventListener('click', () => {
+    let numeros = document.querySelector('#numeros');
+    numeros.innerHTML = '';
+
+    for (let i = 0; i < 20; i++) {
+        const numero = Math.floor(Math.random() * 1000) + 1;
+        numeros.innerHTML += `${numero}<br>`;
+    }
+});
