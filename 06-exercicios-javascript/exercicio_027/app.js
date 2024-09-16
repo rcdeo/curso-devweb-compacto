6a79cd6473c8cfb3fede91d3c660382afae4e015
Rafael Cortes <rafael_cortes@outlook.com>
2024-09-16 10:30:23 -0300

    Exercício 027 - Verificar texto de input e mudar a cor

---
+++ 06-exercicios-javascript/exercicio_027/app.js
@@ -0,0 +1,15 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 027
+Enunciado:
+    Existe um input de texto no qual, sempre que for alterado o seu conteúdo, o 
+    mesmo deve ser apresentado em tempo real no div cujo id é igual a 
+    "conteudo". Contudo, se o texto contém a palavra "olá", a cor do texto deve 
+    ser verde. Caso contrário deverá ser sempre branca.
+---------------------------------------------------------------------------- */
+const conteudo = document.querySelector('#conteudo');
+
+document.querySelector('#text_frase').addEventListener('input', (e) => {
+    conteudo.textContent = e.target.value;
+    conteudo.style.color = e.target.value.includes('olá') ? 'green' : 'white';
+});
