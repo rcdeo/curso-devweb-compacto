68a953aa70c7ebd55466b4a296058367f8beb1f0
Rafael Cortes <rafael_cortes@outlook.com>
2024-10-17 11:46:09 -0300

    Aula 248 - Escrever Código de PHP

---
+++ 08-php8-basico-intermediario/aula248/index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula248</title>
+    
+</head>
+<body>
+    
+    <h1>HTML</h1>
+
+    <?php
+        echo 'PHP';
+    ?>
+
+    <p><?= 'PHP' ?></p>
+
+    <div>
+        <?php
+            print 'Olá mundo!'
+        ?>
+    </div>
+
+</body>
+</html>
