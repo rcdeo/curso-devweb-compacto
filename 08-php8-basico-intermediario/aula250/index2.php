ece749f730b26655ab9c62320cc2612240ae79bd
Rafael Cortes <rafael_cortes@outlook.com>
2024-10-17 16:56:26 -0300

    Aula 250 - Comentários em PHP

---
+++ 08-php8-basico-intermediario/aula250/index2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+
+    <!-- comentário de HTML. Vai aparecer no código fonte -->
+
+    <?php // comentário de PHP. Não vai aparecer no código fonte ?>
+
+    <h1>Teste de comentários</h1>
+
+</body>
+</html>
