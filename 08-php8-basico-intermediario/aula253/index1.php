dad21485cc7d9e68477205144b5a96fc1955c3a8
Rafael Cortes <rafael_cortes@outlook.com>
2024-10-20 17:00:55 -0300

    Aula 253 - Constantes

---
+++ 08-php8-basico-intermediario/aula253/index1.php
@@ -0,0 +1,22 @@
+<?php
+
+// CONSTANTES
+// ao contrário das variáveis, as constantes têm valores que não podem ser alterados.
+
+// definir uma constante
+define('nome', 'valor');
+
+// por convenção, as constantes são definidas com letras maiúsculas
+define('TAXA_FIXA', 10);
+define('APRESENTAR_DADOS', false);
+
+// apresentar o valor de uma constante
+echo TAXA_FIXA; // 10
+
+// usar o valor de uma constante
+$preco_inicial = 500;
+$preco_final = $preco_inicial + TAXA_FIXA;
+echo $preco_final;  // 510
+
+// Não podemos alterar o valor de uma constante
+// TAXA_FIXA = 100;
