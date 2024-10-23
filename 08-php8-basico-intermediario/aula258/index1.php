fadcad96785dcd878f870c30c6bb04e474ae0cbd
Rafael Cortes <rafael_cortes@outlook.com>
2024-10-23 11:54:20 -0300

    Aula 258 - Null

---
+++ 08-php8-basico-intermediario/aula258/index1.php
@@ -0,0 +1,21 @@
+<?php
+
+// NULL
+
+// é um tipo especial de dado.
+// representa uma variável sem valor ou com valor null
+
+$valor = null;  // (case-insensitive)
+echo "valor: $valor";
+var_dump($valor);
+echo '<br>';
+var_dump('teste');
+
+var_dump($numero);
+echo '<br>';
+
+$valor2 = 100;
+echo $valor2;
+unset($valor2);
+echo '<br>';
+var_dump($valor2);
