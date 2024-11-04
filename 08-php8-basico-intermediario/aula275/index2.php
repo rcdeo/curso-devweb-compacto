6c536bcc020ef8b6fe7f1bbf3d5aa36f51e61672
Rafael Cortes <rafael_cortes@outlook.com>
2024-11-04 15:36:16 -0300

    Aula 275 - Instrução Condicional Match

---
+++ 08-php8-basico-intermediario/aula275/index2.php
@@ -0,0 +1,15 @@
+<?php
+
+// EXPRESSÃO MATCH
+
+// Podemos também analisar múltiplas condições de comparação.
+
+$valor = 100;
+
+$resultado = match (true) {
+    $valor > 100 => 'Valor maior que 100.',
+    $valor == 100 => 'Valor igual a 100.',
+    $valor < 100 => 'Valor menor que 100.',
+};
+
+echo $resultado;
