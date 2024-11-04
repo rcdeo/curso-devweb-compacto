93d08bf6df989e74e3ca1262ab3e6bc21c056054
Rafael Cortes <rafael_cortes@outlook.com>
2024-11-04 15:22:16 -0300

    Aula 274 - Instrução Condicional Switch

---
+++ 08-php8-basico-intermediario/aula274/index2.php
@@ -0,0 +1,29 @@
+<?php
+
+// EXPRESSÃO CONDICIONAL SWITCH
+
+// podemos agrupar vários valores em análise para
+// execução do mesmo bloco de código.
+
+$estado_encomenda = 'em processamento';
+
+switch ($estado_encomenda) {
+    case 'em processamento':
+    case 'em análise':
+        // código
+        break;
+
+    case 'anulada':
+    case 'cancelada':
+    case 'inválida':
+        // código
+        break;
+
+    case 'enviada':
+        // código
+        break;
+
+    default:
+        // código
+        break;
+}
---
+++ 08-php8-basico-intermediario/aula274/index3.php
@@ -0,0 +1,15 @@
+<?php
+
+// EXPRESSÃO CONDICIONAL SWITCH
+
+$sexo = 'masculino';
+
+switch ($sexo) {
+    case 'masculino':
+        // código
+        break;
+
+    case 'feminino':
+        // código
+        break;
+}
