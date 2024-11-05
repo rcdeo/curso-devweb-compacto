4811210b468932ff58669b7ab80aa2152fb76e24
Rafael Cortes <rafael_cortes@outlook.com>
2024-11-05 17:30:50 -0300

    Aula 276 - Operador Ternário :bug:

--- 08-php8-basico-intermediario/aula276/index2.php
+++ 08-php8-basico-intermediario/aula276/index2.php
@@ -7,13 +7,13 @@ $erro = true;
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>aula276</title>
     <style>
         .erro {
             background-color: red;
