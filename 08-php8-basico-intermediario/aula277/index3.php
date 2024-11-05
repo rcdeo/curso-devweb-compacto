71bdc21b8dfb46258111ffdcf27d85065d1b22c5
Rafael Cortes <rafael_cortes@outlook.com>
2024-11-05 17:31:15 -0300

    Aula 277 - Ciclos While & Do While :bug:

--- 08-php8-basico-intermediario/aula277/index3.php
+++ 08-php8-basico-intermediario/aula277/index3.php
@@ -5,13 +5,13 @@ $valor2 = 11;
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>aula277</title>
 </head>
 
 <body>
